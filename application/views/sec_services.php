df8992361242118561a4090990cb13bfc7b353e8
Juan A. Delgado <juandel@juandel.fibertel.com.ar>
2016-02-04 00:59:32 -0300

    Services- Adds info for services passed on from the client.

--- application/views/sec_services.php
+++ application/views/sec_services.php
@@ -13,7 +13,8 @@
                     <i class="fa fa-pencil-square-o fa-stack-1x fa-inverse"></i>
                 </span>
                 <h4 class="service-heading">Planificacion</h4>
-                <p class="text-muted">Nos encargamos de planificar el proyecto. Y realizar todos los estudios previos. 
+                <p class="text-muted">Nuestro equipo se encargara de planificar el proyecto, las inversiones y la factibilidad económica, realizando para ello todos los estudios previos necesarios. 
+                        Una correcta planificación nos abre el camino hacia un Proyecto exitoso. 
                 </p>
             </div>
             <div class="col-md-4">
@@ -22,7 +23,7 @@
                     <i class="fa fa-home fa-stack-1x fa-inverse"></i>
                 </span>
                 <h4 class="service-heading">Proyecto</h4>
-                <p class="text-muted">Hacemos proyectos de obra para viviendas unifamiliares, multifamiliares, entre otras. 
+                <p class="text-muted">Realizamos proyectos completos de obras de  viviendas unifamiliares, multifamiliares y de  todo  tipo de obras comerciales, de acuerdo a los códigos municipales correspondientes. Tomando en consideración prioritariamente la calidad de vida, el desarrollo sustentable y la integración al medio ambiente. 
                 </p>
             </div>
             <div class="col-md-4">
@@ -31,7 +32,8 @@
                     <i class="fa fa-building fa-stack-1x fa-inverse"></i>
                 </span>
                 <h4 class="service-heading">Construccion</h4>
-                <p class="text-muted">Tenemos años de experiencia trabajando en la construccion. Desarrollando en unos de los barrios mas importantes de Buenos aires.</p>
+                <p class="text-muted">Tenemos una importante experiencia de más de 45 años trabajando en la construcción, abarcando todas las etapas necesarias de las obras, respetando totalmente las especificaciones técnicas, la calidad y las terminaciones comprometidas en todas nuestras obras, logrando un producto en total armonía con las reglas del arte y con el entorno.
+                </p>
             </div>
         </div>
     </div>
